3380ef7b1311e09afe6ea92cfffd331a4efc37d3
ali <ali@ali.actor>
2020-03-07 06:03:38 +0500

    Added first pass drawing class to draw a polygon

---
+++ src/main/scala/actor/ali/m4p/ch2/Drawing.scala
@@ -0,0 +1,21 @@
+package actor.ali.m4p.ch2
+
+import breeze.linalg._
+import breeze.plot._
+import com.typesafe.scalalogging.Logger
+
+class Drawing {
+
+    private val log = Logger[Drawing]
+
+    val fig: Figure = Figure()
+    val plt: Plot = fig.subplot(0)
+
+    def polygon(points: DenseMatrix[Double]):Unit = {
+        require(points.cols == 2)
+        val x = points(::, 0)
+        val y = points(::, 1)
+        plt += plot(x, y, colorcode = "blue")
+        fig.refresh()
+    }
+}
