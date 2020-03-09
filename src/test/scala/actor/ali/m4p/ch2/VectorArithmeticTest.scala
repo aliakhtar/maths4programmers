e3ea6c304b3dae772a2f39a738236574e94acb34
ali <ali@ali.actor>
2020-03-10 02:59:37 +0500

    expanded translation test

--- src/test/scala/actor/ali/m4p/ch2/VectorArithmeticTest.scala
+++ src/test/scala/actor/ali/m4p/ch2/VectorArithmeticTest.scala
@@ -44,8 +44,20 @@ class VectorArithmeticTest extends org.scalatest.FunSuite {
 
         val translated = VectorArithmetic.translate(translation, orig)
 
-        println("Translated:")
-        println(translated) //verify manually, will take too long to write test
+        //row 1
+        translated(0, 0) should be(1.0)
+        translated(0, 1) should be(1.0)
+
+        //row 2
+        translated(1, 0) should be(1.0)
+        translated(1, 1) should be(2.0)
+
+        //row 3
+        translated(2, 0) should be(-2.0)
+        translated(2, 1) should be(-2.0)
+
+        translated.rows should be(3)
+        translated.cols should be(2)
     }
 
 }
