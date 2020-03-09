4c6fec74ede0042d13ac8115469c74d4c93175b0
ali <ali@ali.actor>
2020-03-09 06:54:19 +0500

    Passing test for VectorArithmetic.scala #add

--- src/main/scala/actor/ali/m4p/ch2/VectorArithmetic.scala
+++ src/main/scala/actor/ali/m4p/ch2/VectorArithmetic.scala
@@ -1,10 +1,20 @@
 package actor.ali.m4p.ch2
 
+import breeze.linalg.DenseVector
 import com.typesafe.scalalogging.Logger
 
 object VectorArithmetic {
 
     private val log = Logger("VectorArithmetic")
 
-    def add(v1: DenseV)
+    def add(v1: DenseVector[Double], v2: DenseVector[Double]):DenseVector[Double] = {
+        require(v1.length == v2.length)
+
+        val copy = v1.copy
+        (0 until v1.length).foreach(i => {
+            copy(i) = v1(i) + v2(i)
+        })
+
+        copy
+    }
 }
---
+++ src/test/scala/actor/ali/m4p/ch2/VectorArithmeticTest.scala
@@ -0,0 +1,25 @@
+package actor.ali.m4p.ch2
+
+import breeze.linalg.DenseVector
+import com.typesafe.scalalogging.Logger
+import org.scalatest.Matchers._
+
+import scala.concurrent.ExecutionContext.Implicits._
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
+class VectorArithmeticTest extends org.scalatest.FunSuite {
+
+    private val log = Logger[VectorArithmeticTest]
+
+    test("add") {
+        val v1 = DenseVector(1.0, 2.0)
+        val v2 = DenseVector(0.1, 0.2)
+
+        val result = VectorArithmetic.add(v1, v2)
+
+        result(0) should be(1.1)
+        result(1) should be(2.2)
+    }
+
+}
