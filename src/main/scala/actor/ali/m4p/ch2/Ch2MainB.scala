1ef03c15259ececdbc3cd71571b2f191d0012bd5
ali <ali@ali.actor>
2020-03-11 04:09:47 +0500

    added Ch2MainB.scala

---
+++ src/main/scala/actor/ali/m4p/ch2/Ch2MainB.scala
@@ -0,0 +1,14 @@
+package actor.ali.m4p.ch2
+
+import com.typesafe.scalalogging.Logger
+
+/**
+ * Contains solutions for Chapter 2, section 2.3
+ *
+ * - polarToCartesian: VectorUtil#polarToCartesian()
+ */
+
+object Ch2MainB extends App {
+
+    private val log = Logger("Ch2MainB")
+}
