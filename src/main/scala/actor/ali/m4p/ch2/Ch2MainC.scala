4300a7b310922ef744faa055eb2c22131aa8bee9
ali <ali@ali.actor>
2020-03-12 02:19:52 +0500

    drawing rotated dino by adding to angle

--- src/main/scala/actor/ali/m4p/ch2/Ch2MainC.scala
+++ src/main/scala/actor/ali/m4p/ch2/Ch2MainC.scala
@@ -1,13 +1,34 @@
 package actor.ali.m4p.ch2
 
+import actor.ali.m4p.util.{Drawing, MatrixUtil, Util, VectorUtil}
 import com.typesafe.scalalogging.Logger
-
+import breeze.linalg._
+import breeze.numerics
+import actor.ali.m4p.util.Util._
 
 
 /**
  * Contains solutions for Chapter 2, section 2.4
+ *
+ * - Draw rotated dino by 45 deg: rotateDino()
  */
 object Ch2MainC extends App {
 
     private val log = Logger("Ch2MainC")
+
+
+    rotateDino()
+
+    def rotateDino() : Unit = {
+        val angle = Pi / 4.0 //45 degrees
+        val dinoRotated = dinoVectors(*, ::).map(v => {
+            val polar = VectorUtil.cartesianToPolar(v)
+            polar(1) = polar(1) + angle
+            VectorUtil.polarToCartesian(polar)
+        })
+
+        val drawing = new Drawing()
+        drawing.polygon2D(dinoVectors)
+        drawing.polygon2D(dinoRotated, lineColor = "red")
+    }
 }
