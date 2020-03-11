167ae231a27f3c05180228dcf80802edfc5bdd9f
ali <ali@ali.actor>
2020-03-12 04:19:57 +0500

    added translating, then rotating

--- src/main/scala/actor/ali/m4p/ch2/Ch2MainC.scala
+++ src/main/scala/actor/ali/m4p/ch2/Ch2MainC.scala
@@ -11,9 +11,10 @@ import actor.ali.m4p.util.Util._
  * Contains solutions for Chapter 2, section 2.4
  *
  * - Draw rotated dino by 45 deg: rotateDino()
- * - Rotate, then translate dino: rotateThenTranslate()
+ * - Rotate, then translate dino: rotateAndTranslate() (also includes translateThenRotate solution)
  * - Ex: rotate() function: MatrixUtil.rotate()
  * - Ex: function to draw an N sided polygon: polygonTest()
+ * - Ex: translate then rotate: rotateAndTranslate()
  */
 object Ch2MainC extends App {
 
@@ -21,8 +22,8 @@ object Ch2MainC extends App {
 
 
     //rotateDino()
-    //rotateThenTranslate()
-    polygonTest()
+    rotateAndTranslate()
+    //polygonTest()
 
     def rotateDino() : Unit = {
         val angle = Pi / 4.0 //45 degrees
@@ -34,13 +35,22 @@ object Ch2MainC extends App {
     }
 
 
-    def rotateThenTranslate():Unit = {
-        val result = MatrixUtil.translate(DenseVector(8.0, 8.0),
-                                            MatrixUtil.rotate(5 * Pi / 3, dinoVectors))
+    def rotateAndTranslate():Unit = {
+
+        val angle = 5 * Pi / 3
+        val t = DenseVector(8.0, 8.0)
+        //Rotate, then translate:
+        val rt = MatrixUtil.translate(t,
+                                            MatrixUtil.rotate(angle, dinoVectors))
+
+
+        //Translate, then rotate:
+        val tr = MatrixUtil.rotate(  angle, MatrixUtil.translate(t, dinoVectors))
 
         val drawing = new Drawing()
         drawing.polygon2D(dinoVectors)
-        drawing.polygon2D(result, lineColor = "red")
+        drawing.polygon2D(rt, lineColor = "red")
+        drawing.polygon2D(tr, lineColor = "black")
     }
 
     def polygonTest():Unit = {
